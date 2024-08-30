c41edca247eefd76ea71844302dce2bbdbc07707
shundayo131 <shundayo.131@gmail.com>
2024-08-29 18:45:25 -0700

    working on config

--- src/main/main.js
+++ src/main/main.js
@@ -1,1 +1,45 @@
-// electron js file 
+// Import 'app' and 'BrowserWindow' module 
+// 'app' controls the application's lifecycle
+// 'BrowserWindow' creates and manages application windows
+const { app, BrowserWindow } = require('electron');
+
+// Include the Node.js 'path' module at the top of your file
+// 'path' is used to handle and transform file paths
+const path = require('node:path');
+
+// Declare a 'createWindow' function that loads 'index.html' into a new BrowserWindow instance
+// This function is responsible for creating the main application window
+const createWindow = () => {
+  // Create a new BrowserWindow instance with specified width and height
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      preload: path.join(__dirname, 'preload.js'), // // Preload script, useful for exposing APIs to the renderer process
+    },
+  })
+
+  // Load the 'index.html' file into the BrowserWindow
+  // The HTML file is located in the 'public' directory, two levels up from the current directory
+  win.loadFile(path.join(__dirname, '..', '..', 'public', 'index.html')); 
+}
+
+// Wait for 'ready' event and invoke createWindow function
+// 'app.whenReady()' ensures that the code runs only when Electron has fully initialized
+app.whenReady().then(() => {
+  createWindow()
+
+  // Open a window if none are open on macOS when the application is activated
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+// Use 'process' globals's platform attribute to run code for each opearting system 
+
+// Handle the 'window-all-closed' event
+// Quit the app when all windows are closed, except on macOS
+// On macOS, it's common for applications to stay active even without open windows
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+});
