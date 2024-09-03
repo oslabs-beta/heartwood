7e7da29c919787300378e915f66e850d81cce375
Yoshimi Araki <yoshimi@Yoshimis-Laptop.local>
2024-09-02 20:45:11 -0400

    github clientId/secret created, still not functioning

--- src/main/main.js
+++ src/main/main.js
@@ -1,7 +1,9 @@
 // Import 'app' and 'BrowserWindow' module 
 // 'app' controls the application's lifecycle
 // 'BrowserWindow' creates and manages application windows
-const { app, BrowserWindow } = require('electron');
+const { app, BrowserWindow, ipcMain } = require('electron');
+const OAuth2 = require('electron-oauth2');
+require('dotenv').config();
 
 // Include the Node.js 'path' module at the top of your file
 // 'path' is used to handle and transform file paths
@@ -11,6 +13,19 @@ const { fork } = require('child_process');
 
 let mainWindow;
 let serverProcess;
+
+
+
+const githubOAuthConfig = {
+    clientId:  process.env.GITHUB_CLIENTID,
+    clientSecret: process.env.GITHUB_CLIENT_SECRET ,
+    authorizationUrl: 'https://github.com/login/oauth/authorize',
+    tokenUrl: 'https://github.com/login/oauth/access_token',
+    useBasicAuthorizationHeader: false,
+    redirectUri: 'http://localhost:8080/home'
+};
+const oauth2 = new OAuth2(githubOAuthConfig);
+//const githubOAuth = oauth2(githubOAuthConfig);
 
 // Declare a 'createWindow' function that loads 'index.html' into a new BrowserWindow instance
 // This function is responsible for creating the main application window
@@ -40,6 +55,7 @@ function startServer() {
 // Wait for 'ready' event and invoke createWindow and startServer function
 // 'app.whenReady()' ensures that the code runs only when Electron has fully initialized
 app.whenReady().then(() => {
+
   createWindow();
   startServer();
 
@@ -47,7 +63,20 @@ app.whenReady().then(() => {
   app.on('activate', () => {
     if (BrowserWindow.getAllWindows().length === 0) createWindow()
   })
+
+
+  ipcMain.handle('start-github-auth', async () => {
+    try {
+        const token = await githubOAuth.getAccessToken({ scope: 'user repo' });
+        return token;
+    } catch (err) {
+        console.error('OAuth error:', err);
+        throw err;
+    }
+  });
 })
+
+
 
 // Use 'process' globals's platform attribute to run code for each opearting system 
 
