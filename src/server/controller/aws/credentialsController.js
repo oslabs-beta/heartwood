795b6debe1050c4b714c100d43682eb3ccc19f2d
shundayo131 <shundayo.131@gmail.com>
2024-09-11 16:55:26 -0700

    Backend to add AWS credentials & Formatted server file

--- src/server/controller/aws/credentialsController.js
+++ src/server/controller/aws/credentialsController.js
@@ -1,30 +1,58 @@
+// -----------------------------------------
+// Imports and Configuration
+// -----------------------------------------
 const User = require('../../models/user');
 
+
+// -----------------------------------------
+// AWS Credential Controller
+// -----------------------------------------
 const awsCredential = {};
 
-// middleware to store AWS credentials for a specified user in database 
+// Middleware to store AWS credentials for a specified user in database 
 awsCredential.addAWSCredential = async (req, res, next) => {
-  // Get User's ssid from cookie (To be confirmed) 
-  const userId = '66d38dd4300649b12a4ca215'; // use temporary id 
+  // TO DO: Get User's ssid from cookie 
+  const userId = '66d38dd4300649b12a4ca215'; // Currently hardcode an id 
 
   // Get Access Key, Secret Access Key, and Region from request body 
-  const [accessKey, secretAccessKey, region] = req.body; 
+  const { accessKey, secretAccessKey, region } = req.body; 
 
   // Error handling - if data is not enough, return error 
-
-
-  // Save AWS credentials to user schema's awsCredential
-  try {
-    // Find a User object 
-    
-    // Update awsCredential property's value
-
-  } catch (error) {
-
+  if (!accessKey || !secretAccessKey || !region) {
+    throw new Error('Missing required properties in request body');
   }
 
-  
+  // Create a newCredentials object to store in database 
+  const newCredentials = {
+    AWS_ACCESS_KEY_ID: accessKey,
+    AWS_SECRET_ACCESS_KEY: secretAccessKey,
+    AWS_REGION: region,
+  }
 
-}
+  try {
+    // Find a User object and update awsCredential property 
+    const updatedUser = await User.findByIdAndUpdate(
+      userId,
+      { $set: { awsCredential: newCredentials } },
+      { new: true, runValidators: true }
+    );
 
-Module.exports = awsCredential;
+    // Throw an error if user is not found 
+    if (!updatedUser) {
+      throw new Error('User not found');
+    };
+
+    // Next middleware
+    return next();
+
+  } catch (error) {
+    // Pass error object to global error handler
+    const errObj = {
+      log: `failed to add AWS credential: ${err}`,
+      message: { err: 'failed to add AWS credential' },
+    };
+    return next(errObj);
+  };
+};
+
+module.exports = awsCredential;
