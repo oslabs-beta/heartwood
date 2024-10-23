a41fa23a029363a7ad594e10c64f3c3b074e2838
Jackie <jackielau94@gmail.com>
2024-10-22 22:29:24 -0400

    fixedthislongsecret

--- src/server/controller/aws/functionListController.ts
+++ src/server/controller/aws/functionListController.ts
@@ -54,7 +54,7 @@ const getLambdaFunctions = {
             region: AWS_REGION,
             credentials: {
               accessKeyId: AWS_ACCESS_KEY_ID,
-              secretAccessKey: AWS_SECRET_ACCESS_KEY ,
+              secretAccessKey: AWS_SECRET_ACCESS_KEY,
             }
         });
         const input = {
