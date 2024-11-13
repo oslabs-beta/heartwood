1ffdbfcf3f628df4b516694b2c744efb2811b1a6
shundayo131 <shundayo.131@gmail.com>
2024-11-12 18:40:09 -0800

    packaging

--- forge.config.js
+++ forge.config.js
@@ -41,4 +41,17 @@ module.exports = {
       [FuseV1Options.OnlyLoadAppFromAsar]: true,
     }),
   ],
+  publishers: [
+    {
+      name: '@electron-forge/publisher-github',
+      config: {
+        repository: {
+          owner: 'oslabs-beta',
+          name: 'heartwood'
+        },
+        prerelease: false,
+        draft: true
+      }
+    }
+  ]
 };
